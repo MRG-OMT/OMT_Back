95d1295e9ad1ddc2acf7508e58677d4e0522a827
MRG-OMT <mrg.omtool@gmail.com>
2025-09-16 17:40:03 +0530

    Added district field in place model

--- models/places.js
+++ models/places.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 const placesSchema = new mongoose.Schema({
+    district:{
+        type:String
+    },
     place:{
         type:String,
         required:true,
